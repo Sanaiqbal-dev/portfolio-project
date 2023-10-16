785a58f40f792d5b2b09944aa724f973b880ab39
Sana Iqbal <sana.iqbal@codeninjaconsulting.com>
2023-10-17 00:51:11 +0500

    Task 3 in prorgess

--- portfolio-form-submission.js
+++ portfolio-form-submission.js
@@ -1,64 +1,105 @@
 const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-document.addEventListener("DOMContentLoaded", function () {
+let expItemNumber = 1;
 
-  var form = document.getElementById("submit-form");
+function SubmitFunc(event) {
+  event.preventDefault();
+  let userName = document.getElementById("name");
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
 
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
+  let invalidNameAlert = document.getElementById("alert-name");
+  let invalidEmailAlert = document.getElementById("alert-email");
+  let invalidPasswordAlert = document.getElementById("alert-password");
 
-      let userName = document.getElementById("name");
-      let email = document.getElementById("email");
-      let password = document.getElementById("password");
+  let successMsg = document.getElementById("success-msg");
 
-      let alertName = document.getElementById("alert-name");
-      let alertEmail = document.getElementById("alert-email");
-      let alertPassword = document.getElementById("alert-password");
+  let isUserNameValid,
+    isEmailValid,
+    isPasswordValid = false;
 
-      let successMsg = document.getElementById("success-msg");
-
-      let isUserName,
-        isEmail,
-        isPassword = false;
-
-      if (userName.value.length == 0) {
-        userName.style.border = "1px solid red";
-        alertName.style.display = "block";
-        isUserName = false;
-      } else {
-        userName.style.border = "1px solid grey";
-        alertName.style.display = "none";
-        isUserName = true;
-      }
-
-      if (regex.test(email.value) == false) {
-        email.style.border = "1px solid red";
-        alertEmail.style.display = "block";
-        isEmail = false;
-      } else {
-        email.style.border = "1px solid grey";
-        alertEmail.style.display = "none";
-        isEmail = true;
-      }
-
-      if (password.value.length <= 7) {
-        password.style.border = "1px solid red";
-        alertPassword.style.display = "block";
-        isPassword = false;
-      } else {
-        password.style.border = "1px solid grey";
-        alertPassword.style.display = "none";
-        isPassword = true;
-      }
-
-      if (isUserName && isEmail && isPassword) {
-        successMsg.style.display = "block";
-        successMsg.scrollIntoView({ behavior: "smooth" });
-
-      } else {
-        successMsg.style.display = "none";
-      }
-    });
+  if (userName.value.length == 0) {
+    userName.style.borderColor = "red";
+    invalidNameAlert.style.display = "block";
+    isUserNameValid = false;
+  } else {
+    userName.style.borderColor = "grey";
+    invalidNameAlert.style.display = "none";
+    isUserNameValid = true;
   }
-});
+
+  if (regex.test(email.value) == false) {
+    email.style.borderColor = "red";
+    invalidEmailAlert.style.display = "block";
+    isEmailValid = false;
+  } else {
+    email.style.borderColor = "grey";
+    invalidEmailAlert.style.display = "none";
+    isEmailValid = true;
+  }
+
+  if (password.value.length <= 7) {
+    password.style.borderColor = "red";
+    invalidPasswordAlert.style.display = "block";
+    isPasswordValid = false;
+  } else {
+    password.style.borderColor = "grey";
+    invalidPasswordAlert.style.display = "none";
+    isPasswordValid = true;
+  }
+
+  if (isUserNameValid && isEmailValid && isPasswordValid) {
+    successMsg.style.display = "block";
+    successMsg.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function closeForm(event) {
+  event.preventDefault();
+  document.querySelector(".work-exp-form").style.display = "none";
+  document.querySelector(".add-exp").style.display = "block";
+
+}
+function AddWorkExperience() {
+  document.querySelector(".work-exp-form").style.display = "flex";
+  document.querySelector(".add-exp").style.display = "none";
+}
+
+function ShowWorkExp(event){
+  event.preventDefault();
+
+  let workExpItem = document.createElement("div");
+  workExpItem.className = "exp-item";
+
+  let companyName = document.createElement("h4");
+  let jobDuration = document.createElement("h5");
+  jobDuration.className = "exp-content";
+  let jobDescription = document.createElement("p");
+  jobDescription.className = "exp-content";
+
+  companyName.textContent = expItemNumber + ". " + document.getElementById("company-name").value;
+  let startDate = new Date(document.getElementById("start-date").value);
+  startDate = startDate.toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
+  let  endDate = new Date(document.getElementById("end-date").value);
+  endDate = endDate.toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
+  
+  jobDuration.textContent = startDate + "      " + endDate;
+  jobDescription.textContent = document.getElementById("description").value;
+
+  workExpItem.appendChild(companyName);
+  workExpItem.appendChild(jobDuration);
+  workExpItem.appendChild(jobDescription);
+  document.querySelector(".exp-list").appendChild(workExpItem);
+
+  resetForm();
+  document.querySelector(".work-exp-form").style.display = "none";
+  document.querySelector(".add-exp").style.display = "block";
+  expItemNumber +=1 ; 
+}
+function resetForm(){
+  document.getElementById("company-name").value = "";
+  document.getElementById("start-date").value = "";
+  document.getElementById("end-date").value = "";
+  document.getElementById("description").value = "";
+
+}
