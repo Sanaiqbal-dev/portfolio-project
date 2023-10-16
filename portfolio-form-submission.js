a06231b35bf54b525ff9d136e3c945b5107887bd
Sana Iqbal <sana.iqbal@codeninjaconsulting.com>
2023-10-16 10:41:27 +0500

    completed requested changes by reviewer

--- portfolio-form-submission.js
+++ portfolio-form-submission.js
@@ -1,62 +1,53 @@
 const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-document.addEventListener("DOMContentLoaded", function () {
-  var form = document.getElementById("submit-form");
+function SubmitFunc(event) {
+  event.preventDefault();
+  let userName = document.getElementById("name");
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
 
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
+  let invalidNameAlert = document.getElementById("alert-name");
+  let invalidEmailAlert = document.getElementById("alert-email");
+  let invalidPasswordAlert = document.getElementById("alert-password");
 
-      let userName = document.getElementById("name");
-      let email = document.getElementById("email");
-      let password = document.getElementById("password");
+  let successMsg = document.getElementById("success-msg");
 
-      let alertName = document.getElementById("alert-name");
-      let alertEmail = document.getElementById("alert-email");
-      let alertPassword = document.getElementById("alert-password");
+  let isUserNameValid,
+    isEmailValid,
+    isPasswordValid = false;
 
-      let successMsg = document.getElementById("success-msg");
-
-      let isUserName,
-        isEmail,
-        isPassword = false;
-
-      if (userName.value.length == 0) {
-        userName.style.border = "1px solid red";
-        alertName.style.display = "block";
-        isUserName = false;
-      } else {
-        userName.style.border = "1px solid grey";
-        alertName.style.display = "none";
-        isUserName = true;
-      }
-
-      if (regex.test(email.value) == false) {
-        email.style.border = "1px solid red";
-        alertEmail.style.display = "block";
-        isEmail = false;
-      } else {
-        email.style.border = "1px solid grey";
-        alertEmail.style.display = "none";
-        isEmail = true;
-      }
-
-      if (password.value.length <= 7) {
-        password.style.border = "1px solid red";
-        alertPassword.style.display = "block";
-        isPassword = false;
-      } else {
-        password.style.border = "1px solid grey";
-        alertPassword.style.display = "none";
-        isPassword = true;
-      }
-
-      if (isUserName && isEmail && isPassword) {
-        successMsg.style.display = "block";
-        successMsg.scrollIntoView({ behavior: "smooth" });
-      } else {
-        successMsg.style.display = "none";
-      }
-    });
+  if (userName.value.length == 0) {
+    userName.style.borderColor = "red";
+    invalidNameAlert.style.display = "block";
+    isUserNameValid = false;
+  } else {
+    userName.style.borderColor = "grey";
+    invalidNameAlert.style.display = "none";
+    isUserNameValid = true;
   }
-});
+
+  if (regex.test(email.value) == false) {
+    email.style.borderColor = "red";
+    invalidEmailAlert.style.display = "block";
+    isEmailValid = false;
+  } else {
+    email.style.borderColor = "grey";
+    invalidEmailAlert.style.display = "none";
+    isEmailValid = true;
+  }
+
+  if (password.value.length <= 7) {
+    password.style.borderColor = "red";
+    invalidPasswordAlert.style.display = "block";
+    isPasswordValid = false;
+  } else {
+    password.style.borderColor = "grey";
+    invalidPasswordAlert.style.display = "none";
+    isPasswordValid = true;
+  }
+
+  if (isUserNameValid && isEmailValid && isPasswordValid) {
+    successMsg.style.display = "block";
+    successMsg.scrollIntoView({ behavior: "smooth" });
+  }
+}
